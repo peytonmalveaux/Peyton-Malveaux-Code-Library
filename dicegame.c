a7789469b630b9f43af4d8a37cb2abf31bb6f20c
peytonmalveaux <127824491+peytonmalveaux@users.noreply.github.com>
2023-03-18 20:11:27 -0700

    Delete dicegame.c

--- dicegame.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "dicegame.h"
-
-void printPlayerPoints(int a, int* p1, int* p2){
-    printf("Player 1 points: %d\nPlayer 2 points: %d\n", *p1, *p2);
-
-}
